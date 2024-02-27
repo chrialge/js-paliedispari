6fbdd5f5f391ea42c6bf6d23ed6bc6ee1850a636
chrialge <chrialge@gmail.com>
2024-02-27 16:19:05 +0100

    add function palidroma

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,36 @@
 console.log("ciao")
 
 
-const word = 'ciao';
-const arrayWord = word.split('');
-console.log(arrayWord);
-let letterByContrast = arrayWord.reverse();
-console.log(letterByContrast)
-const wordByContrast = letterByContrast.join('');
-console.log(wordByContrast);
 
-if (wordByContrast = word){
-    alert('la tua parola e palidroma')
-}else{
-    alert('la tua parola non e palidroma')
+// const arrayWord = word.split('');
+
+// let letterByContrast = arrayWord.reverse();
+
+// const wordByContrast = letterByContrast.join('');
+
+
+// if (wordByContrast == word){
+//     alert('la tua parola e palidroma')
+// }else{
+//     alert('la tua parola non e palidroma')
+// }
+const word = prompt('Inserisci solo una parola:');
+
+palidromaWord(word)
+function palidromaWord(word){
+    const arrayWord = word.split('');
+
+    let letterByContrast = arrayWord.reverse();
+
+    const wordByContrast = letterByContrast.join('');
+
+
+    if (wordByContrast == word){
+        alert('la tua parola e palidroma')
+        
+    }else{
+        alert('la tua parola non e palidroma')
+        
+    }
+
 }
