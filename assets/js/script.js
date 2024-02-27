6e53fa3f3c5b978ab58958021465e04ded640dd6
chrialge <chrialge@gmail.com>
2024-02-27 16:37:03 +0100

    add structure pair and dispair without function

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,23 +14,33 @@ console.log("ciao")
 // }else{
 //     alert('la tua parola non e palidroma')
 // }
-const word = prompt('Inserisci solo una parola:');
+// const word = prompt('Inserisci solo una parola:');
 
-palidromaWord(word)
-function palidromaWord(word){
-    const arrayWord = word.split('');
+// palidromaWord(word)
+// function palidromaWord(word){
+//     const arrayWord = word.split('');
 
-    let letterByContrast = arrayWord.reverse();
+//     let letterByContrast = arrayWord.reverse();
 
-    const wordByContrast = letterByContrast.join('');
+//     const wordByContrast = letterByContrast.join('');
 
 
-    if (wordByContrast == word){
-        alert('la tua parola e palidroma')
+//     if (wordByContrast == word){
+//         alert('la tua parola e palidroma')
         
-    }else{
-        alert('la tua parola non e palidroma')
+//     }else{
+//         alert('la tua parola non e palidroma')
         
-    }
+//     }
 
-}
+// }
+
+
+const pair = prompt('vuoi il numero pari o dispari','pari o dispari');
+console.log(typeof pair);
+const numb = Number(prompt('inserisci un numero tra 1 e 5'));
+console.log(numb);
+const numbRandom = Math.floor(Math.random() * 5) + 1
+console.log(numbRandom);
+const sum = numb + numbRandom;
+console.log(sum);
