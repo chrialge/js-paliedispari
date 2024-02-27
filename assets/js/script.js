d581083aa5fe39310a26b7af7dbdd73bfaed01bd
chrialge <chrialge@gmail.com>
2024-02-27 15:42:05 +0100

    commit trial

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
 console.log("ciao")
+
+
+const word = 'anna';
+const arrayWord = word.split('');
+console.log(arrayWord);
+let wordByContrast = [];
+let lenght = Number(arrayWord.length -1);
+console.log(lenght); 
+
+for (let i = 3; 3 >= 0; i --) {
+    const element = arrayWord[i];
+    console.log('ciao');
+    wordByContrast.push(element)
+}
+console.log(wordByContrast)
